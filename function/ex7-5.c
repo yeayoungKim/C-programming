333353266a4ebc3ce870addc02a2a0ff34f34891
yeayoung Kim <sweetpotato@jj.ac.kr>
2024-11-06 09:07:04 +0900

    FIX: Functions with no return value
    Functions with no return value
    
    -반환값이 없는 함수

--- function/ex7-5.c
+++ function/ex7-5.c
@@ -4,7 +4,7 @@ void print_char(char ch, int count);// 함수 선언
 
 int main(void)
 {
-    print_char('@',5); // 문자와 숫자를 주고 함수 호출
+    print_char('@',4); // 문자와 숫자를 주고 함수 호출
 
     return 0;
 }
@@ -17,5 +17,5 @@ void print_char(char ch,int count) // 매개변수는 있으나 반환형은 없
     {
         printf("%C",ch); //매개변수 ch에 받은 문자 출력 
     }
-    return;
+    return; 
 }
